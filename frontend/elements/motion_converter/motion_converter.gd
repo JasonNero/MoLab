93071cf6fe38beaf2c04929088c8ff93a9d389da
Jason Schuehlein <jason@schuehlein.eu>
2025-01-03 12:34:50 +0100

    implement animation -> packed_motion

--- frontend/elements/motion_converter/motion_converter.gd
+++ frontend/elements/motion_converter/motion_converter.gd
@@ -26,6 +26,8 @@ var _joint_order: Array[String] = [
 	"RightHand"
 ]
 
+# var order: int = EULER_ORDER_YZX
+var order: int = EULER_ORDER_ZYX
 
 func results_to_animations(results: InferenceResults) -> Array[Animation]:
 	var animations: Array[Animation] = []
@@ -54,15 +56,13 @@ func results_to_animations(results: InferenceResults) -> Array[Animation]:
 			anim.track_set_path(track_idx_joint, "%GeneralSkeleton:{0}".format([joint_name]))
 
 			for key in range(key_count):
-				# TODO: GO ON HERE
-
 				var euler: Vector3 = Vector3(
 					deg_to_rad(results.joint_rotations[variant_idx][key][joint_id][0]),
 					deg_to_rad(results.joint_rotations[variant_idx][key][joint_id][1]),
 					deg_to_rad(results.joint_rotations[variant_idx][key][joint_id][2])
 				)
 
-				var basis: Basis = Basis.from_euler(euler, EULER_ORDER_YZX)
+				var basis: Basis = Basis.from_euler(euler, order)
 				var quat: Quaternion = basis.get_rotation_quaternion()
 
 				# var quat: Quaternion = Quaternion.from_euler(euler)
@@ -74,5 +74,40 @@ func results_to_animations(results: InferenceResults) -> Array[Animation]:
 	return animations
 
 
-# func animation_to_packed_motion(anim: Animation) -> Dictionary:
-# 	pass
+func animation_to_packed_motion(anim: Animation) -> Dictionary:
+	# This returns a mapping of frame_idx to a list of joint rotations
+	# E.g.:
+	# {
+	# 	1: [
+	# 		[0, 0, 0],
+	# 		[90, 0, 0],
+	# 		...
+	# 		[170, 45, -90]
+	# 	],
+	# 	2: ...
+	# }
+
+	var frame_count = int(anim.length * Globals.FPS)
+	assert(frame_count < 197, "Frame count exceeds 197, this is not supported by the model.")
+
+	var hip_pos_track: int = anim.find_track(NodePath("%GeneralSkeleton:Hips"), Animation.TYPE_POSITION_3D)
+
+	var packed_motion: Dictionary = {}
+	for frame in range(frame_count):
+		var frame_data: Array = []
+
+		var hip_pos_key: int = anim.track_find_key(hip_pos_track, frame / Globals.FPS, Animation.FIND_MODE_APPROX)
+		var hip_pos: Vector3 = anim.track_get_key_value(hip_pos_track, hip_pos_key)
+		frame_data.append([hip_pos.x, hip_pos.y, hip_pos.z])
+
+		for bone_name in _joint_order:
+			var bone_track: int = anim.find_track(NodePath("%GeneralSkeleton:{0}".format([bone_name])), Animation.TYPE_ROTATION_3D)
+			var bone_key: int = anim.track_find_key(bone_track, frame / Globals.FPS, Animation.FIND_MODE_APPROX)
+			var quat: Quaternion = anim.track_get_key_value(bone_track, bone_key)
+			var euler: Vector3 = quat.get_euler(order)
+
+			frame_data.append([rad_to_deg(euler.x), rad_to_deg(euler.y), rad_to_deg(euler.z)])
+
+		packed_motion[frame] = frame_data
+
+	return packed_motion
