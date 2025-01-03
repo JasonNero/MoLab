40efd788d2d9962ac00705a4587b9538bf6355bc
Jason Schuehlein <jason@schuehlein.eu>
2025-01-03 12:30:05 +0100

    move apply function to specialize it to the source type

--- frontend/datamodels/source.gd
+++ frontend/datamodels/source.gd
@@ -43,100 +43,13 @@ func set_property(property: String, value: Variant) -> void:
 
 
 func apply(target_animation: Animation) -> Animation:
-	if not animation:
-		print("Skipping source without animation: ", name)
-		return target_animation
-
-	# Source properties are in [frames], Animations in [seconds]
-	var in_point_sec := float(in_point) / Globals.FPS
-	var out_point_sec := float(out_point) / Globals.FPS
-	var in_offset_sec := float(in_offset) / Globals.FPS
-	var out_offset_sec := float(out_offset) / Globals.FPS
-
-	var override_start_sec := in_point_sec + in_offset_sec
-	var override_end_sec := out_point_sec - out_offset_sec
-
-	var trimmed_animation := _trim_and_center_animation()
-
-	var target_hip_idx := target_animation.find_track(NodePath("%GeneralSkeleton:Hips"), Animation.TYPE_POSITION_3D)
-	var hip_offset: Vector3 = Vector3.ZERO
-	if target_hip_idx != -1:
-		# Get the root offset at the start of the override range
-		hip_offset = target_animation.position_track_interpolate(target_hip_idx, override_start_sec - 1.0/Globals.FPS)
-		hip_offset.y = 0  # Ignore vertical offset
-
-	for source_track_idx in trimmed_animation.get_track_count():
-		var track_type := trimmed_animation.track_get_type(source_track_idx)
-		var track_path := trimmed_animation.track_get_path(source_track_idx)
-
-		var target_track_idx := _get_or_create_track(target_animation, track_path, track_type)
-
-		# Make sure we disable "wrapping" to prevent sliding keyframes
-		target_animation.track_set_interpolation_loop_wrap(target_track_idx, false)
-
-		# Remove keyframes in the target range before overwriting
-		# Use reverse order to prevent index shifting
-		var key_count := target_animation.track_get_key_count(target_track_idx)
-		var to_remove := []
-		for key_idx in key_count:
-			var key_time := target_animation.track_get_key_time(target_track_idx, key_idx)
-			if key_time >= override_start_sec and key_time <= override_end_sec:
-				to_remove.append(key_idx)
-		to_remove.reverse()
-		for key_idx in to_remove:
-			target_animation.track_remove_key(target_track_idx, key_idx)
-
-		# Copy and offset all keyframes
-		key_count = trimmed_animation.track_get_key_count(source_track_idx)
-		for key_idx in key_count:
-			var local_time := trimmed_animation.track_get_key_time(source_track_idx, key_idx)
-
-			# Skip keyframes outside the override range
-			if local_time < in_offset_sec or local_time > out_point_sec - in_point_sec - out_offset_sec:
-				continue
-
-			var value = trimmed_animation.track_get_key_value(source_track_idx, key_idx)
-			var global_time := local_time + in_point_sec
-
-			# Root/Hip Motion offset
-			if target_track_idx == target_hip_idx:
-				value += hip_offset
-
-			target_animation.track_insert_key(
-				target_track_idx,
-				global_time,
-				value,
-				# transition,
-			)
-	# affects_post_range = false
-	if affects_post_range:
-		target_hip_idx = target_animation.find_track(NodePath("%GeneralSkeleton:Hips"), Animation.TYPE_POSITION_3D)
-		hip_offset = Vector3.ZERO
-		if target_hip_idx != -1:
-			# Get the root offset at the end of the trimmed animation
-			hip_offset = trimmed_animation.position_track_interpolate(target_hip_idx, trimmed_animation.length)
-			hip_offset.y = 0  # Ignore vertical offset
-
-			# Now also apply the Hip offset to all following keyframes
-			for key_idx in target_animation.track_get_key_count(target_hip_idx):
-				var key_time := target_animation.track_get_key_time(target_hip_idx, key_idx)
-				if key_time < override_end_sec:
-					continue
-				var key_value: Vector3 = target_animation.track_get_key_value(target_hip_idx, key_idx)
-				var new_value := key_value + hip_offset
-				new_value.y = key_value.y  # Ignore vertical offset
-				target_animation.track_set_key_value(target_hip_idx, key_idx, new_value)
-
+	print("Base Source has no apply method.")
 	return target_animation
 
 
 # Trim the source animation to the inner range and let it start at the origin.
-func _trim_and_center_animation() -> Animation:
+func _trim_and_center_animation(in_offset_sec: float, out_offset_sec: float) -> Animation:
 	var trimmed_animation := Animation.new()
-
-	# Source properties are in [frames], Animations in [seconds]
-	var in_offset_sec := float(in_offset) / Globals.FPS
-	var out_offset_sec := float(out_offset) / Globals.FPS
 
 	# Trimming the animation to the override range
 	for track_idx in animation.get_track_count():
@@ -175,15 +88,17 @@ func _trim_and_center_animation() -> Animation:
 	var hip_track_idx := trimmed_animation.find_track(NodePath("%GeneralSkeleton:Hips"), Animation.TYPE_POSITION_3D)
 	var hip_key_count = trimmed_animation.track_get_key_count(hip_track_idx)
 
-	if hip_track_idx == -1 or hip_key_count == 0:
-		print("Source '", name, "' has no Hip Translation keyframes.")
+	if hip_track_idx == -1:
+		print("Source has not Hip Translation track.")
+	elif hip_key_count == 0:
+		print("Source has no Hip Translation keyframes.")
 	else:
 		# Take the first XZ position keyframe and subtract it from all keyframes
 		var first_key_idx := trimmed_animation.track_find_key(hip_track_idx, 0, Animation.FIND_MODE_APPROX)
 		var first_key_value: Vector3
 		if first_key_idx == -1:
 			# Fall back to interpolated value fetched at time 0
-			push_warning("Using interpolated position for Hip start for source '", name, "'")
+			push_warning("Using interpolated position for Hip start for source.")
 			first_key_value = trimmed_animation.position_track_interpolate(hip_track_idx, 0)
 		else:
 			first_key_value = trimmed_animation.track_get_key_value(hip_track_idx, first_key_idx)
@@ -195,6 +110,7 @@ func _trim_and_center_animation() -> Animation:
 			new_value.y = key_value.y  # Ignore vertical offset
 			trimmed_animation.track_set_key_value(hip_track_idx, key_idx, new_value)
 
+	print("Animation trimmed to range: ", in_offset_sec, " - ", animation.length - out_offset_sec)
 	return trimmed_animation
 
 
--- frontend/datamodels/source_file.gd
+++ frontend/datamodels/source_file.gd
@@ -22,3 +22,90 @@ func get_properties() -> Dictionary:
 	}
 
 	return props
+
+func apply(target_animation: Animation) -> Animation:
+	if not animation:
+		print("Skipping source without animation: ", name)
+		return target_animation
+
+	# Source properties are in [frames], Animations in [seconds]
+	var in_point_sec := float(in_point) / Globals.FPS
+	var out_point_sec := float(out_point) / Globals.FPS
+	var in_offset_sec := float(in_offset) / Globals.FPS
+	var out_offset_sec := float(out_offset) / Globals.FPS
+
+	var override_start_sec := in_point_sec + in_offset_sec
+	var override_end_sec := out_point_sec - out_offset_sec
+
+	var trimmed_animation := _trim_and_center_animation(in_offset_sec, out_offset_sec)
+
+	var target_hip_idx := target_animation.find_track(NodePath("%GeneralSkeleton:Hips"), Animation.TYPE_POSITION_3D)
+	var hip_offset: Vector3 = Vector3.ZERO
+	if target_hip_idx != -1:
+		# Get the root offset at the start of the override range
+		hip_offset = target_animation.position_track_interpolate(target_hip_idx, override_start_sec - 1.0/Globals.FPS)
+		hip_offset.y = 0  # Ignore vertical offset
+
+	for source_track_idx in trimmed_animation.get_track_count():
+		var track_type := trimmed_animation.track_get_type(source_track_idx)
+		var track_path := trimmed_animation.track_get_path(source_track_idx)
+
+		var target_track_idx := _get_or_create_track(target_animation, track_path, track_type)
+
+		# Make sure we disable "wrapping" to prevent sliding keyframes
+		target_animation.track_set_interpolation_loop_wrap(target_track_idx, false)
+
+		# Remove keyframes in the target range before overwriting
+		# Use reverse order to prevent index shifting
+		var key_count := target_animation.track_get_key_count(target_track_idx)
+		var to_remove := []
+		for key_idx in key_count:
+			var key_time := target_animation.track_get_key_time(target_track_idx, key_idx)
+			if key_time >= override_start_sec and key_time <= override_end_sec:
+				to_remove.append(key_idx)
+		to_remove.reverse()
+		for key_idx in to_remove:
+			target_animation.track_remove_key(target_track_idx, key_idx)
+
+		# Copy and offset all keyframes
+		key_count = trimmed_animation.track_get_key_count(source_track_idx)
+		for key_idx in key_count:
+			var local_time := trimmed_animation.track_get_key_time(source_track_idx, key_idx)
+
+			# Skip keyframes outside the override range
+			if local_time < in_offset_sec or local_time > out_point_sec - in_point_sec - out_offset_sec:
+				continue
+
+			var value = trimmed_animation.track_get_key_value(source_track_idx, key_idx)
+			var global_time := local_time + in_point_sec
+
+			# Root/Hip Motion offset
+			if target_track_idx == target_hip_idx:
+				value += hip_offset
+
+			target_animation.track_insert_key(
+				target_track_idx,
+				global_time,
+				value,
+				# transition,
+			)
+	# affects_post_range = false
+	if affects_post_range:
+		target_hip_idx = target_animation.find_track(NodePath("%GeneralSkeleton:Hips"), Animation.TYPE_POSITION_3D)
+		hip_offset = Vector3.ZERO
+		if target_hip_idx != -1 and target_animation.track_get_key_count(target_hip_idx) > 0:
+			# Get the root offset at the end of the trimmed animation
+			hip_offset = trimmed_animation.position_track_interpolate(target_hip_idx, trimmed_animation.length)
+			hip_offset.y = 0  # Ignore vertical offset
+
+			# Now also apply the Hip offset to all following keyframes
+			for key_idx in target_animation.track_get_key_count(target_hip_idx):
+				var key_time := target_animation.track_get_key_time(target_hip_idx, key_idx)
+				if key_time < override_end_sec:
+					continue
+				var key_value: Vector3 = target_animation.track_get_key_value(target_hip_idx, key_idx)
+				var new_value := key_value + hip_offset
+				new_value.y = key_value.y  # Ignore vertical offset
+				target_animation.track_set_key_value(target_hip_idx, key_idx, new_value)
+
+	return target_animation
